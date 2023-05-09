e25b0ac11f4926550d70942ae1503af451168983
gol1bjon_jorayev <jorayevdev002@gmail.com>
2023-05-09 12:20:26 +0500

    first commit

---
+++ src/main/java/developer/jorayev/springboot/SpringBootNewApplication.java
@@ -0,0 +1,13 @@
+package developer.jorayev.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/developer/jorayev/springboot/SpringBootNewApplicationTests.java
@@ -0,0 +1,13 @@
+package developer.jorayev.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
